1bea319c10de158015859f1540ead02265e888f8
yangyunhan <1205385190@qq.com>
2018-01-11 14:56:51 +0800

    config file of webpack product

---
+++ webpack.production.config.js
@@ -0,0 +1,49 @@
+//webpack.production.config.js
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle-[hash].js"
+    },
+    devtool: "null",
+    module: {
+        rules: [{
+            test: /(\.jsx|\.js)$/,
+            use: {
+                loader: "babel-loader"
+            },
+            exclude: /node_modules/
+        },{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        modules: ture
+                    }
+                }]
+            })
+        }]
+    },
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css"),
+
+        new CleanWebpackPlugin('build/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
+    ]
+};
